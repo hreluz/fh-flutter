30b847d613db22f06d13d788ac074bd7349da224
Hector <hmreluz@gmail.com>
2026-01-13 18:48:10 -0500

    01-first-steps: Material Design 3

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -10,8 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue
+      ),
       home: CounterScreen()
     );
   }
