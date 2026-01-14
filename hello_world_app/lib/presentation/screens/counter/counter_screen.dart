3dbfab23f1251c1a1ee54be362515a13b1b9c0bf
Hector <hmreluz@gmail.com>
2026-01-13 20:00:29 -0500

    01-first-steps: change word click (s)

--- hello_world_app/lib/presentation/screens/counter/counter_screen.dart
+++ hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+
+  int clickCounter = 0;
+  String text = 'Clicks';
 
   @override
   Widget build(BuildContext context) {
@@ -12,14 +21,19 @@ class CounterScreen extends StatelessWidget {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text('10', style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
-              Text('Clicks', style: TextStyle(fontSize: 25))
+            children: [
+              Text('$clickCounter', style: const TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
+              Text('$text', style: const TextStyle(fontSize: 25))
             ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              clickCounter++;
+              text = clickCounter == 1 ? 'Click' : 'Clicks';
+            });
+          },
           child: const Icon(Icons.plus_one),
         ),
       );
