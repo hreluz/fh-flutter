08fd62972bfe3083bdbe72a7837a97b404726cf3
Hector <hmreluz@gmail.com>
2026-01-19 19:11:08 -0500

    02-yes-no-app: Other User messages - Bubble chat

--- yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_messages_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/other_message_bubble.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -33,7 +34,7 @@ class _ChatView extends StatelessWidget {
             children: [
               Expanded(
                 child: ListView.builder(itemCount:100, itemBuilder: (context,index) {
-                  return MyMessageBubble();
+                  return index % 2 == 0 ? const MyMessageBubble() : const OtherMessageBubble();
                 })
               ),
               Text('World')
