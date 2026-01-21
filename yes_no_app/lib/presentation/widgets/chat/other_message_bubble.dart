fd19383c8c6eaeeac7ead95da3e69ca181ed3dc6
Hector <hmreluz@gmail.com>
2026-01-21 16:59:59 -0500

    02-yes-no-app: showing loading message when image is loading

--- yes_no_app/lib/presentation/widgets/chat/other_message_bubble.dart
+++ yes_no_app/lib/presentation/widgets/chat/other_message_bubble.dart
@@ -43,7 +43,16 @@ class _ImageBubble extends StatelessWidget {
         "https://yesno.wtf/assets/no/1-c7d128c95c1740ec76e120146c870f0b.gif",
         width: size.width * 0.7,
         height: 150,
-        fit: BoxFit.cover
+        fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+          return Container(
+            width: size.width * 0.7,
+            height: 150,
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: const Text("Batman is sending an image ..."),
+          );
+        },
       )
       );
   }
