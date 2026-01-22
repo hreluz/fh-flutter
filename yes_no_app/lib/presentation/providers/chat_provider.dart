97aeb03699ee4322cea8dddaa76804f024a27d80
Hector <hmreluz@gmail.com>
2026-01-21 19:00:23 -0500

    02-yes-no-app: show message provider

--- yes_no_app/lib/presentation/providers/chat_provider.dart
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messageList = [
     Message(text: 'Hello there', fromWho: FromWho.me),
     Message(text: 'Where are you?', fromWho: FromWho.notMe),
   ];
